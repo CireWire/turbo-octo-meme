7d05e5737684d767f540ecc05ba05eb5c7188642
CireWire <djcire956@gmail.com>
2017-05-03 12:28:55 -0500

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,17 @@
+def plugs (x):
+  print "Calculating..."
+  print (x ** 3) + 12
+  print "**********************************"
+  print "This is your answer!"
+  
+print plugs (3)
+
+
+def argName(name, role):
+  print "We are looking for..."
+  print "Name:", name 
+  print "Occupation:", role
+  print "***********************************"
+  print "Have you seen him/her?"
+  
+print argName("Tyler", "Mechanic")
